478526bf5479aa2127ce53e4bcb7894d912a1281
watercolor-bluejayy <footballgirl78000@gmail.com>
2024-03-19 21:16:47 -0400

    center obstacle and bang counter added. As well as exit after 3 bangs

--- WattmanJayy_Lab7_CGT215.cpp
+++ WattmanJayy_Lab7_CGT215.cpp
@@ -18,7 +18,7 @@ int main()
 
     //create the ball so we have something to bounce
     PhysicsCircle ball;
-    ball.setCenter(Vector2f(400, 300)); //remember that in SFPhysics we orient from the center
+    ball.setCenter(Vector2f(400, 400)); //remember that in SFPhysics we orient from the center
     ball.setRadius(20); //set the size of the ball
     world.AddPhysicsBody(ball); //adds ball to world so it can be affected by the gravity and such
 
@@ -31,13 +31,13 @@ int main()
 
     //make the walls and ceiling
     PhysicsRectangle LWall;
-    LWall.setSize(Vector2f(20, 600));
+    LWall.setSize(Vector2f(20, 560));
     LWall.setCenter(Vector2f(790, 300));
     LWall.setStatic(true);
     world.AddPhysicsBody(LWall);
 
     PhysicsRectangle RWall;
-    RWall.setSize(Vector2f(20, 600));
+    RWall.setSize(Vector2f(20, 560));
     RWall.setCenter(Vector2f(10, 300));
     RWall.setStatic(true);
     world.AddPhysicsBody(RWall);
@@ -48,15 +48,71 @@ int main()
     ceiling.setStatic(true);
     world.AddPhysicsBody(ceiling);
 
+    //add center obstacle
+    PhysicsRectangle obstacle;
+    obstacle.setSize(Vector2f(90, 90));
+    obstacle.setCenter(Vector2f(400, 300));
+    obstacle.setStatic(true);
+    world.AddPhysicsBody(obstacle);
+
+    //ball needs more velocity
+    ball.applyImpulse(Vector2f(-0.005, -0.7));
+
     int thudCount(0);
-    floor.onCollision = [&thudCount](PhysicsBodyCollisionResult result) // use an anonymus function with a reference to thudCount
+    floor.onCollision = [&thudCount](PhysicsBodyCollisionResult result) // use an lambda function with a reference to thudCount
         {
             cout << "thud " << thudCount << endl;
             thudCount++; //increment the thud count
         };
 
+    //we want a thud for the walls and ceiling too, so implement similar lambda functions
+    LWall.onCollision = [&thudCount](PhysicsBodyCollisionResult result) // use an lambda function with a reference to thudCount
+        {
+            cout << "thud " << thudCount << endl;
+            thudCount++; //increment the thud count
+        };
+    RWall.onCollision = [&thudCount](PhysicsBodyCollisionResult result) // use an lambda function with a reference to thudCount
+        {
+            cout << "thud " << thudCount << endl;
+            thudCount++; //increment the thud count
+        };
+    ceiling.onCollision = [&thudCount](PhysicsBodyCollisionResult result) // use an lambda function with a reference to thudCount
+        {
+            cout << "thud " << thudCount << endl;
+            thudCount++; //increment the thud count
+        };
+
+    //to differentiate between walls and the obstacle a new "sound" is needed
+    int bangCount(0);
+    obstacle.onCollision = [&bangCount, &obstacle, &window](PhysicsBodyCollisionResult result) 
+        {
+            cout << "bang " << bangCount << endl;
+            bangCount++; 
+
+            //we want the program to exit after the ball hits the obstacle 3 times
+            if (bangCount == 3) 
+            {
+                Clock simpleClock;
+                Time start(simpleClock.getElapsedTime());
+                Time simpleDelta(start - start); //start at 0
+
+                while (simpleDelta.asMilliseconds() < 2000) //this will turn the obstacle blue for 2s before closing
+                {
+                    Time currTime = simpleClock.getElapsedTime();
+                    simpleDelta = currTime - start;
+
+                    obstacle.setFillColor(Color(20, 0, 255));
+                    window.draw(obstacle);
+                    window.display();
+                }
+
+                exit(0);
+            }
+        };
+
     Clock clock;
     Time lastTime(clock.getElapsedTime());
+
     while (true) 
     {
         //calculate milliseconds since last frame
@@ -74,6 +130,7 @@ int main()
         window.draw(LWall);
         window.draw(RWall);
         window.draw(ceiling);
+        window.draw(obstacle);
         window.display();
     }
 }
